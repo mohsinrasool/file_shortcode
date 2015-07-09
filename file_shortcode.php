e45671194b32438208ca081cd4977afe8d90e643
mohsinrasool <mohsin.rasool@gmail.com>
2015-07-09 17:15:06 +0500

    Create file_shortcode.php

---
+++ file_shortcode.php
@@ -0,0 +1,280 @@
+<?php
+/**
+ * Plugin Name:       File Shortcodes
+ * Description:       This plugin provides you two shortcodes and made their output editable through WP plugin editor
+ * Version:           1.0
+ * Author:            Mohsin Rasool
+ * License:           GPL2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
+/**
+ *
+ *
+ */
+class FiverrShortcodes {
+
+	/**
+	 * Plugin version
+	 *
+	 * @since   1.0.0
+	 *
+	 * @var     string
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 *
+	 * Unique identifier for plugin.
+	 *
+	 * The variable name is used as the text domain when internationalizing strings
+	 * of text. Its value should match the Text Domain file header in the main
+	 * plugin file.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      string
+	 */
+	protected $plugin_slug = 'fiverr_shortcodes';
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      object
+	 */
+	protected static $instance = null;
+
+
+	/**
+	 * Initialize the plugin by setting localization and loading public scripts
+	 * and styles.
+	 *
+	 * @since     1.0.0
+	 */
+	private function __construct() {
+		$this->install_shortcodes();
+		add_filter('widget_text', 'do_shortcode');
+	}
+
+	/**
+	 * Return the plugin slug.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return    Plugin slug variable.
+	 */
+	public function get_plugin_slug() {
+		return $this->plugin_slug;
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since     1.0.0
+	 *
+	 * @return    object    A single instance of this class.
+	 */
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    boolean    $network_wide    True if WPMU superadmin uses
+	 *                                       "Network Activate" action, false if
+	 *                                       WPMU is disabled or plugin is
+	 *                                       activated on an individual blog.
+	 */
+	public static function activate( $network_wide ) {
+ 
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+
+			if ( $network_wide  ) {
+
+				// Get all blog ids
+				$blog_ids = self::get_blog_ids();
+
+				foreach ( $blog_ids as $blog_id ) {
+					switch_to_blog( $blog_id );
+					self::single_activate();
+				}
+
+				restore_current_blog();
+
+			} else {
+				self::single_activate();
+			}
+
+		} else {
+			self::single_activate();
+		}
+
+	}
+
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    boolean    $network_wide    True if WPMU superadmin uses
+	 *                                       "Network Deactivate" action, false if
+	 *                                       WPMU is disabled or plugin is
+	 *                                       deactivated on an individual blog.
+	 */
+	public static function deactivate( $network_wide ) {
+
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+
+			if ( $network_wide ) {
+
+				// Get all blog ids
+				$blog_ids = self::get_blog_ids();
+
+				foreach ( $blog_ids as $blog_id ) {
+
+					switch_to_blog( $blog_id );
+					self::single_deactivate();
+
+				}
+
+				restore_current_blog();
+
+			} else {
+				self::single_deactivate();
+			}
+
+		} else {
+			self::single_deactivate();
+		}
+
+	}
+
+	/**
+	 * Fired when a new site is activated with a WPMU environment.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    int    $blog_id    ID of the new blog.
+	 */
+	public function activate_new_site( $blog_id ) {
+
+		if ( 1 !== did_action( 'wpmu_new_blog' ) ) {
+			return;
+		}
+
+		switch_to_blog( $blog_id );
+		self::single_activate();
+		restore_current_blog();
+
+	}
+
+	/**
+	 * Get all blog ids of blogs in the current network that are:
+	 * - not archived
+	 * - not spam
+	 * - not deleted
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return   array|false    The blog ids, false if no matches.
+	 */
+	private static function get_blog_ids() {
+
+		global $wpdb;
+
+		// get an array of blog ids
+		$sql = "SELECT blog_id FROM $wpdb->blogs
+			WHERE archived = '0' AND spam = '0'
+			AND deleted = '0'";
+
+		return $wpdb->get_col( $sql );
+
+	}
+
+	/**
+	 * Fired for each blog when the plugin is activated.
+	 *
+	 * @since    1.0.0
+	 */
+	private static function single_activate() {
+
+		return true;
+	}
+
+	/**
+	 * Fired for each blog when the plugin is deactivated.
+	 *
+	 * @since    1.0.0
+	 */
+	private static function single_deactivate() {
+
+		return true;
+	}
+
+
+	/**
+	 * Add shortcodes 
+	 *
+	 * @since    1.0.0
+	 */
+	public function install_shortcodes()
+	{
+		
+		add_shortcode( 'shortcode_1', array(&$this, 'shortcode_1') );
+		add_shortcode( 'shortcode_2', array(&$this, 'shortcode_2') );
+	}
+
+
+
+	/**
+	 * Implementation of shortcode_1
+	 *
+	 * @since    1.0.0
+	 */
+	function shortcode_1($attr)
+	{
+		$FileLocation = WP_PLUGIN_DIR."/fiverr_shortcodes/shortcode_1.html";
+		if(file_exists($FileLocation)) {
+			return file_get_contents($FileLocation);
+		}
+		else 
+			return 'Shortcode file do not exists';
+	}
+
+	/**
+	 * Implementation of shortcode_2
+	 *
+	 * @since    1.0.0
+	 */
+	function shortcode_2($attr)
+	{
+		$FileLocation = WP_PLUGIN_DIR."/fiverr_shortcodes/shortcode_2.html";
+		if(file_exists($FileLocation)) {
+			return file_get_contents($FileLocation);
+		}
+		else 
+			return 'Shortcode file do not exists';
+	}
+}
+
+register_activation_hook( __FILE__, array( 'FiverrShortcodes', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'FiverrShortcodes', 'deactivate' ) );
+
+add_action( 'plugins_loaded', array( 'FiverrShortcodes', 'get_instance' ) );
